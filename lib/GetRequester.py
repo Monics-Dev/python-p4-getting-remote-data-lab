e892ebbbe6cb886ba59e92eec6fcebec689106e1
Monica <monijerobon@gmail.com>
2023-06-21 17:01:21 +0300

    complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,19 @@
 import requests
 import json
-
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
         pass
+        response=requests.get(self.url)
+        return response.content
+
 
     def load_json(self):
         pass
+        python_list=[]
+        programs=json.loads(self.get_response_body())
+        for program in programs:
+            python_list.append(program)
+        return python_list
